266547fdbe4ca6bef7ddba34a53715e882ba0746
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-27 12:39:15 -0400

    fix: remove storing in memory

--- docs/nftLoader/src/lib.rs
+++ docs/nftLoader/src/lib.rs
@@ -5,7 +5,6 @@ mod block_timestamp;
 
 use pb::transfers;
 use pb::transfers::transfer::Schema;
-use substreams::store::{self, DeltaProto, StoreNew, StoreSetIfNotExists, StoreSetIfNotExistsProto};
 use substreams_database_change::pb::database::{table_change::Operation, DatabaseChanges};
 use substreams_ethereum::pb as ethpb;
 
@@ -82,6 +81,7 @@ fn transform_block_to_transfers(blk: ethpb::eth::v2::Block) -> (BlockTimestamp, 
     (timestamp, transfers)
 }
 
+#[allow(dead_code)]
 fn new_erc20_transfer(
     hash: &[u8],
     ordinal: u32,
@@ -230,11 +230,26 @@ fn schema_to_string(schema: Schema) -> String {
     .to_string()
 }
 
-/// Parses block and saves to store
-#[substreams::handlers::store]
-fn store_transfers(blk: ethpb::eth::v2::Block, s: StoreSetIfNotExistsProto<transfers::Transfer>) {
+#[substreams::handlers::map]
+fn db_out(
+    blk: ethpb::eth::v2::Block
+) -> Result<DatabaseChanges, substreams::errors::Error> {
     let (_timestamp, transfers) = transform_block_to_transfers(blk);
 
+    let mut database_changes: DatabaseChanges = Default::default();
+
+    // for loop over transfers
+    for transfer in transfers {
+        transform_erc721_transfers_to_database_changes(&mut database_changes, transfer);
+    }
+
+    Ok(database_changes)
+}
+
+fn transform_erc721_transfers_to_database_changes(
+    changes: &mut DatabaseChanges,
+    transfer: transfers::Transfer,
+) {
     let unique_key = |transfer: &transfers::Transfer| {
         format!(
             "{}-{}-{}-{}-{}-{}",
@@ -247,47 +262,14 @@ fn store_transfers(blk: ethpb::eth::v2::Block, s: StoreSetIfNotExistsProto<trans
         )
     };
 
-    // for loop over transfers
-    for transfer in transfers {
-        log::info!("Found a transfer {}", unique_key(&transfer));
-        s.set_if_not_exists(transfer.ordinal, unique_key(&transfer), &transfer);
-    }
-}
+    log::info!("Found a transfer {}", unique_key(&transfer));
 
-#[substreams::handlers::map]
-fn db_out(
-    erc_transfer_start: store::Deltas<DeltaProto<transfers::Transfer>>,
-) -> Result<DatabaseChanges, substreams::errors::Error> {
-    let mut database_changes: DatabaseChanges = Default::default();
-    transform_erc721_transfers_to_database_changes(&mut database_changes, erc_transfer_start);
-    Ok(database_changes)
-}
-
-fn transform_erc721_transfers_to_database_changes(
-    changes: &mut DatabaseChanges,
-    deltas: store::Deltas<DeltaProto<transfers::Transfer>>,
-) {
-    use substreams::pb::substreams::store_delta::Operation;
-
-    for delta in deltas.deltas {
-        match delta.operation {
-            Operation::Create => push_create(
-                changes,
-                &delta.key,
-                delta.ordinal,
-                delta.new_value,
-            ),
-            Operation::Update => push_update(
-                changes,
-                &delta.key,
-                delta.ordinal,
-                delta.old_value,
-                delta.new_value,
-            ),
-            Operation::Delete => todo!(),
-            x => panic!("unsupported opeation {:?}", x),
-        }
-    }
+    push_create(
+        changes,
+        &unique_key(&transfer),
+        transfer.ordinal,
+        transfer,
+    )
 }
 
 fn push_create(
@@ -309,38 +291,4 @@ fn push_create(
         .change("tx_hash", (None, Hex(value.tx_hash)))
         .change("ordinal", (None, value.ordinal))
         .change("timestamp", (None, value.timestamp.unwrap()));
-}
-
-fn push_update(
-    changes: &mut DatabaseChanges,
-    key: &str,
-    ordinal: u64,
-    old_value: transfers::Transfer,
-    new_value: transfers::Transfer,
-) {
-    changes
-        .push_change("transfers", key, ordinal, Operation::Update)
-        .change("schema", (old_value.schema, new_value.schema))
-        .change("block_number", (old_value.block_number, new_value.block_number))
-        .change("from_address", (Hex(old_value.from_address), Hex(new_value.from_address)))
-        .change("to_address", (Hex(old_value.to_address), Hex(new_value.to_address)))
-        .change("operator", (Hex(old_value.operator), Hex(new_value.operator)))
-        .change("quantity", (old_value.quantity, new_value.quantity))
-        .change(
-            "token_id",
-            (Hex(old_value.token_id), Hex(new_value.token_id)),
-        )
-        .change("contract_address", (Hex(old_value.contract_address), Hex(new_value.contract_address)))
-        .change(
-            "tx_hash",
-            (Hex(old_value.tx_hash), Hex(new_value.tx_hash)),
-        )
-        .change(
-            "ordinal",
-            (old_value.ordinal, new_value.ordinal),
-        )
-        .change(
-            "timestamp",
-            (&old_value.timestamp.unwrap(), &new_value.timestamp.unwrap()),
-        );
 }
