284d0f02c3e4bf7fb9bf0238bb444feb557ae015
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-29 06:03:55 -0400

    fix: add sql statements

--- example_consumer/index.ts
+++ example_consumer/index.ts
@@ -13,6 +13,8 @@ client.query('LISTEN transfers');
 console.log('Waiting for new inserts...');
 
 client.on('notification', (msg: any) => {
-  console.log(`Received notification: ${msg.channel} ${msg.payload}`);
-  console.log(`New row data: ${msg.payload}`);
+  console.log(`Received notification: ${msg.channel}`);
+  const [schema, blockNumber, tokenId, contractAddress, quantity, fromAddress, toAddress, txHash, timestamp] = msg.payload.split("|");
+
+  console.log(`${schema} found at block ${blockNumber}, contract=${contractAddress}, tokenId=${tokenId}, from=${fromAddress}, to=${toAddress}, ${quantity > 1 ? `quantity=${quantity}, ` : ''}txHash=${txHash}, timestamp=${timestamp}`)
 });
